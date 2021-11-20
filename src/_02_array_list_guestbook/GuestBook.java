914e14a15c38668bcdb732f7d317730b7aaf20a6
nolanbooth <56123783+nolanbooth@users.noreply.github.com>
2021-11-19 16:55:49 -0800

    Update GuestBook.java

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,12 +1,65 @@
 package _02_array_list_guestbook;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+import javax.swing.JButton;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class GuestBook implements ActionListener {
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
+	JButton addName = new JButton();
+	JButton viewName = new JButton();
+	JButton terminateHuman = new JButton();
+	int h = 0;
+	ArrayList<String> names  = new ArrayList<String>();
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
+	// Guest #1: Bob Handers
 	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+	// Guest #3: Greg Sanders
+	// Guest #4: Downy Boners
+
+	public void run() {
+		frame.add(panel);
+		panel.add(viewName);
+		panel.add(addName);
+		panel.add(terminateHuman);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addName.addActionListener(this);
+		viewName.addActionListener(this);
+		terminateHuman.addActionListener(this);
+		addName.setText("add name");
+		viewName.setText("view names");
+		terminateHuman.setText("T E R M I N A T E   H U M A N");
+		frame.pack();
+	
+	names.add("Bob Handers");
+	names.add("Sandy Summers");
+	names.add("Greg Sanders");
+	names.add("Downy Boners");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == viewName) {
+			JOptionPane.showMessageDialog(null, "Loading... Viewing Names...");
+		for(String s : names) {
+			System.out.println(s);
+		}
+		System.out.println(" - - - - - - - - - - - - - - - -");
+		
+		}if(e.getSource() == addName) {
+			JOptionPane.showMessageDialog(null, "Loading... Getting List...");
+			String hh = JOptionPane.showInputDialog("What name you want to add?");
+			names.add(hh);
+		}
+	}
+
 }
